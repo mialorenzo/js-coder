b6de5ea1446214bcd6fde0ad78105faf94ed3454
MiaLorenzo <mia_lorenzo@hotmail.com>
2021-09-17 10:17:02 -0300

    inicio js

---
+++ Clase1/app.js
@@ -0,0 +1,10 @@
+var numero1= 10
+
+let numero2 = Number(prompt('ingresa tu numero'));
+
+
+console.log(numero1 + numero2);
+console.log(numero1 - numero2);
+console.log(numero1 / numero2);
+console.log(numero1 * numero2);
+
