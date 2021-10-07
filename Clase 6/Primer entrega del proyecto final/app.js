fa9b35501b4908b7326c14b770e580abf45a9798
MiaLorenzo <mia_lorenzo@hotmail.com>
2021-10-06 21:39:06 -0300

    pre entrega#

--- Clase 6/Primer entrega del proyecto final/app.js
+++ Clase 6/Primer entrega del proyecto final/app.js
@@ -1,15 +1,4 @@
-// ===== NAVBAR ===== //
-$(window).scroll(function() {
-  
-    let position    =   $(this).scrollTop();
-      if (position >= 100) {
-        $('.nav-menu').addClass('costum-navbar');
-      } else {
-        $('.nav-menu').removeClass('costum-navbar');
-      }
-    
-  });
-  
+
   
   // ===== HAMBURGER BUTTON ===== //
   $(document).ready(function() {
@@ -22,3 +11,45 @@ $(window).scroll(function() {
   
 
   // ==== END NAVBAR ==== //
+
+//=== formulario ===//
+class Persona {
+  constructor(nombre, email, message){
+      this.nombre =nombre;
+      this.email=email; 
+      this.message=message; 
+  }
+}
+
+
+let datoUsuario = []; 
+
+
+datoUsuario.push(new Persona ("Maria Perez", "mariaperez@gmail.com", "hola"))
+datoUsuario.push(new Persona ("Juana Lopez","juana@gmail.com" , "hola como "))
+datoUsuario.push(new Persona ("Felipe Gimenez", "felipe@gmail.com", "hola como estas?"))
+
+
+const agregarPersonasALaLista = () => {
+  let nombre = prompt ("Put your name");
+  let email = prompt("Put your email");
+  let message = prompt( "Leave your message")
+
+
+  let persona = new Persona( nombre, email, message)
+  datoUsuario.push(persona)
+
+}
+
+datoUsuario.sort(( a,b) => {
+  if (a.nombre > b.nombre) {
+      return 1 
+  }
+  if (a.nombre < b.nombre){
+      return -1 
+  }  
+  return 0 
+
+})
+
+console.log(datoUsuario);
