1830fe316fef6db1d901fded4c69afe7933a019b
MiaLorenzo <mia_lorenzo@hotmail.com>
2021-10-13 16:33:22 -0300

    clase 8 DOM

---
+++ clase 8/app.js
@@ -0,0 +1,25 @@
+///formulario
+
+class datos {
+    constructor({name, email, phone, message }) {
+        this.name = name;
+        this.email = email;
+        this.phone =phone;
+        this.message=message;
+    }
+}
+
+
+const guardarInfo = () => {
+
+    const dato = new dato (
+        {
+            name :document.getElementById("name").value,
+            email:document.getElementById("email").value,
+            phone:document.getElementById("phone").value,
+            message:document.getElementById("message").value,
+        })
+
+    return dato
+    
+}
